51441875af84da0cd24ea1c8573943bbcf86c146
ortegake <ortegake@us.ibm.com>
2019-12-18 15:32:53 -0500

    add debug

--- hello.py
+++ hello.py
@@ -51,6 +51,8 @@ def root():
 @app.route('/api/visitors', methods=['GET'])
 def get_visitor():
     if client:
+    	print('Where are you?')
+    	print(map(lambda doc: doc['name'],db))
         return jsonify(list(map(lambda doc: doc['name'], db)))
     else:
         print('No database')
